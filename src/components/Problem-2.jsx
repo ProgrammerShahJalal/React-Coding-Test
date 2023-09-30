8fb1f54dbe72f623aec6e85fd0e6aac8a0a3f183
ProgrammerShahJalal <you@example.com>
2023-09-30 09:06:29 +0600

    modal added

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const Problem2 = () => {
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
     return (
-
+       <>
         <div className="container">
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
                 
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                <button onClick={handleShow} className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
+                <button onClick={handleShow} className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
                 </div>
-                
             </div>
         </div>
+
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+       </>
     );
 };
 
